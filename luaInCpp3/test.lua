2257e3aeda73381506fec0ed309bf098a165c506
Cyrus Hanlon <cyrushanlon@hotmail.com>
2017-10-08 14:43:16 +0100

    removed old folders and added report

--- luaInCpp3/test.lua
+++
@@ -1,6 +0,0 @@
-logout("one argument")
-logout("hello", "this", "is", "many", "args", "with", "a", "number:", 1 )
-
-function add ( x, y )
-	return x + y
-end
--- sfml2/src/Global.cpp
+++
@@ -1,4 +0,0 @@
-#include "Global.h"
-
-std::vector<Drawable*> Drawables;
-std::vector<Point*> Points;
