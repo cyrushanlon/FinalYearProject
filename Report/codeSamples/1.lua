ff47b951c9a1e27bfd5cdffb7149daaa3cca69f3
Cyrus Hanlon <cyrushanlon@hotmail.com>
2017-10-20 18:26:07 +0100

    Restructured Lua section and wrote more

--- Report/codeSamples/1.lua
+++ Report/codeSamples/1.lua
@@ -1,5 +1,5 @@
 --Inline declaration of a table
-local tab1 = {
+local tab = {
     foo= 45,
     tab = {
         x = 23
@@ -8,18 +8,54 @@ local tab1 = {
 }
 
 --Declaration of an example member-like function
-function tab1:func(y)
+function tab:func(y)
     print(self.tab.x + y)
 end
 
-tab1:func(25)
+tab:func(25)
 
-function tab1.func(self, y)
+function tab.func(self, y)
     print(self.tab.x + y)
 end
 
-tab1.func(tab1, 25)
+tab1.func(tab, 25)
 
-print(tab1.place) 			 --prints out 10
-print(tab1["place"])         --prints out 10
-print(tab1.tab["x"])         --prints out 23
+print(tab.place) 			 --prints out 10
+print(tab["place"])         --prints out 10
+print(tab.tab["x"])         --prints out 23
+
+--declaration of a table using numerical indices
+local array = {
+    "this is a string",
+    105,
+    function(x) print("this is a function"..x) end
+} 
+
+print(array[1])             --prints out this is a string
+print(array[2])             --prints out 105
+array[3]("!")               --prints out this is a function!
+
+local tab = {
+    foo = "this is a string",
+    105,
+    "this is another string",
+    [333] = function(x) print("this is a function"..x) end
+}
+
+for k,v in pairs(tab) do 
+    print(k, v) 
+end
+--[[ prints out the following:
+1	105
+2	this is another string
+333	function: 0x1663b90
+foo	this is a string
+]]
+
+for k,v in ipairs(tab) do 
+    print(k, v) 
+end
+--[[ prints out the following:
+1	105
+2	this is another string
+]]
