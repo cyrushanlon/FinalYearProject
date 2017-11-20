99bb7d62edc9c7af453315157ede69db0bae3e31
Cyrus Hanlon <cyrushanlon@hotmail.com>
2017-11-20 15:20:42 +0000

    Wrote some about # and vm and C api

--- Report/codeSamples/1.cpp
+++ Report/codeSamples/1.cpp
@@ -1,8 +1,12 @@
-#include<stdio.h>
-#include<iostream>
-// A comment
-int main(void)
+
+static int l_sin (lua_State *L) 
 {
-    printf("Hello World\n");
-    return 0;
+    double d = lua_tonumber(L, 1);  /* get argument */
+    lua_pushnumber(L, sin(d));  /* push result */
+    return 1;  /* number of results */
+}
+void registerSin(lua_State *L) 
+{
+    lua_pushcfunction(l, l_sin);
+    lua_setglobal(l, "mysin");
 }
